375cbc658ad35917663395d77796a3b77f2da690
Owen Foreman <owen4man@gmail.com>
2022-03-29 14:00:23 -0400

    apikey

--- assets/script.js
+++ assets/script.js
@@ -3,11 +3,14 @@ var mapQuestApiKey = "pz5UbtNqHGRFZY8OHcGGAeZmK7sxjab5";
 var cityString = "Atlanta";
 var buttonLocation = document.getElementById("placeSearch")
 // sort = drop down
+// enter city/zip then enter what you are looking for ie grocery store
+// then gives closest locations 
 
 function getMapApi() {
   // replace `octocat` with anyone else's GitHub username
   var requestUrl = "https://api.github.com/users/octocat/repos";
-  cityString = document.getElementById("enterLocation").textContent;
+  cityString = document.getElementById("enterLocation").value;
+  console.log({cityString});
   fetch(
     "https://www.mapquestapi.com/search/v4/place?q=" + cityString + "&key= "+ mapQuestApiKey + "&sort=relevance"
   )
