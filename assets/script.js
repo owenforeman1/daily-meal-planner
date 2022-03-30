7c001558081e919a34c24b0ad6b0a8f9524b42bb
Dom <Domalbano35@gmail.com>
2022-03-30 13:58:47 -0400

    added dropdown button on inputfield

--- assets/script.js
+++ assets/script.js
@@ -66,3 +66,14 @@ function nextPage() {
 
 btnSwitch.addEventListener('click', nextPage)
 
+
+var dropdown = document.querySelector(".dropdown");
+
+dropdown.addEventListener("click", function (event) {
+  event.stopPropagation();
+  dropdown.classList.toggle("is-active");
+  
+});
+
+
+
